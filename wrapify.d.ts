a2fff5efc860530d6f00cb1e32339d0e3572a870
Brian M. Workman <1266935+gesslar@users.noreply.github.com>
2025-11-26 02:09:26 -0500

    trying

---
+++ wrapify.d.ts
@@ -0,0 +1,4 @@
+export function wrapify(text?: string, maxLineLength?: number, indent?: number): string
+export function iwrapify(text?: string, maxLineLength?: number, indent?: number): string
+
+export default wrapify
